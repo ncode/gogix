0771dc56ac53069a55ebcd188bfd733841b9cb29
Juliano Martinez <juliano@martinez.io>
2012-09-23 21:06:27 -0300

    added syslog logging on fail

--- source/logix/util/check.go
+++ source/logix/util/check.go
@@ -12,12 +12,13 @@ var Cfg *config.Config
 func CheckPanic(err error, message string) {
   if err != nil {
     msg := fmt.Sprintf("%s: %s", message, err)
-    _log, err := syslog.New(syslog.LOG_ERR, "[go]")
+    _log, err := syslog.New(syslog.LOG_ERR, "logix")
     if err != nil {
         log.Fatalln("Unable to write syslog message")
     }
+    _log.Warning(msg)
     defer _log.Close()
     log.Fatalln(msg)
-    panic(fmt.Sprintf(msg))
+    // panic(msg)
   }
 }
