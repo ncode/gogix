6e7b389ccc66d83beb715bea806294d3613637de
Juliano Martinez <juliano@martinez.io>
2013-08-13 14:46:04 -0300

    better panic control

--- utils/check.go
+++ utils/check.go
@@ -6,16 +6,35 @@ import (
 	"log/syslog"
 )
 
+var (
+	_log, s_err = syslog.New(syslog.LOG_ERR, "gogix")
+)
+
+func Check(err error, message string) {
+	check(err, message, false)
+}
+
 func CheckPanic(err error, message string) {
+	check(err, message, true)
+}
+
+func Log(message string) {
+	CheckPanic(s_err, "Unable to write syslog message")
+	_log.Info(message)
+	defer _log.Close()
+}
+
+func check(err error, message string, _panic bool) {
 	if err != nil {
 		msg := fmt.Sprintf("%s: %s", message, err)
-		_log, err := syslog.New(syslog.LOG_ERR, "gogix")
-		if err != nil {
+		if s_err != nil {
 			log.Fatalln("Unable to write syslog message")
 		}
 		_log.Warning(msg)
 		defer _log.Close()
 		log.Fatalln(msg)
-		// panic(msg)
+		if _panic {
+			panic(msg)
+		}
 	}
 }
