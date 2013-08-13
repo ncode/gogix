28c825f77d0b58faa6ba93b4a3471aecf8792084
Juliano Martinez <juliano@martinez.io>
2013-08-13 15:37:58 -0300

    Cleaning package build and improving connection usage

--- Makefile
+++ Makefile
@@ -1,20 +1,20 @@
 .PHONY: build install
 DESTDIR=$(CURDIR)/debian/gogix
-SRCDIR=$(CURDIR)/src
 
 clean:
 	rm --preserve-root -rf $(DESTDIR)
 
 build:
+	go get github.com/ncode/gogix
 	go get github.com/streadway/amqp
 	go get github.com/msbranco/goconfig
-	cd $(SRCDIR) && go build -o gogix-server
+	go build -o gogix
 
 install:
 	install -d --mode=755 $(DESTDIR)
 	install -d --mode=755 $(DESTDIR)/usr/sbin
 	install -d --mode=755 $(DESTDIR)/etc/gogix
 	install -d --mode=755 $(DESTDIR)/etc/rsyslog.d
-	install -v --mode=755 $(SRCDIR)/gogix-server $(DESTDIR)/usr/sbin/gogix-server
-	install -v --mode=644 $(SRCDIR)/config/gogix.conf $(DESTDIR)/etc/gogix/gogix.conf
-	install -v --mode=644 $(SRCDIR)/config/gogix-rsyslog.conf $(DESTDIR)/etc/rsyslog.d/gogix-rsyslog.conf
+	install -v --mode=755 gogix-server $(DESTDIR)/usr/sbin/gogix-server
+	install -v --mode=644 config/gogix.conf $(DESTDIR)/etc/gogix/gogix.conf
+	install -v --mode=644 config/gogix-rsyslog.conf $(DESTDIR)/etc/rsyslog.d/gogix-rsyslog.conf
--- gogix-server.go
+++ gogix-server.go
@@ -13,7 +13,6 @@ import (
 )
 
 var (
-	queue string
 	Cfg   *goconfig.ConfigFile
 	user  = flag.String("u", "gogix", "username")
 	debug = flag.Bool("d", false, "debug")
@@ -28,16 +27,14 @@ func main() {
 		config_file = "/etc/gogix/gogix.conf"
 	}
 
-	var uri string
-
 	Cfg, err = goconfig.ReadConfigFile(config_file)
 	utils.CheckPanic(err, "File not found")
 
 	bind_addr, err := Cfg.GetString("server", "bind_addr")
 	utils.CheckPanic(err, "Unable to get bind_addr from gogix.conf")
-	queue, err = Cfg.GetString("transport", "queue")
+	queue, err := Cfg.GetString("transport", "queue")
 	utils.CheckPanic(err, "Unable to get queue from gogix.conf")
-	uri, err = Cfg.GetString("transport", "uri")
+	uri, err := Cfg.GetString("transport", "uri")
 	utils.CheckPanic(err, "Unable to get transport from gogix.conf")
 	message_ttl, err := Cfg.GetString("transport", "message_ttl")
 	utils.CheckPanic(err, "Unable to get message_ttl from gogix.conf")
@@ -47,34 +44,35 @@ func main() {
 	utils.CheckPanic(err, fmt.Sprintf("Unable to bind %s", addr))
 
 	var conn broker.Connection
-	conn = conn.Dial(uri)
 	if *debug == true {
 		fmt.Printf("Connecting to Broker %s\n", uri)
+	}
+	conn = conn.Dial(uri)
+	conn = conn.SetupBroker(queue, message_ttl)
+	if *debug == true {
+		fmt.Printf("Setting queue %s\n", queue)
 	}
 
 	for {
 		recv := make([]byte, 1024)
 		_, _, err := l.ReadFromUDP(recv)
-		utils.CheckPanic(err, "Problem receiving data")
+		utils.Check(err, "Problem receiving data")
 		go handle_data(string(recv), message_ttl, conn)
 	}
+
+	defer conn.Close()
 }
 
 func handle_data(data string, message_ttl string, conn broker.Connection) {
-	parsed := syslog.ParseLog(data)
-	var c broker.Connection
 
 	if *debug == true {
 		fmt.Printf("Received log %s\n", data)
 	}
 
-	if *debug == true {
-		fmt.Printf("Setup queue %s\n", queue)
-	}
+	parsed := syslog.ParseLog(data)
 
-	c = conn.SetupBroker(queue, message_ttl)
 	if *debug == true {
 		fmt.Printf("Sending data %s\n", parsed)
 	}
-	c.Send(parsed)
+	conn.Send(parsed)
 }
